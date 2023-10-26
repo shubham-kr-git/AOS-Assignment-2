83e20b9265614c2d0d6b275d3c555851275dd601
shubham-kr-git <59856307+shubham-kr-git@users.noreply.github.com>
2023-10-26 18:04:29 +0530

    fixed the reader stream waiting for input stream

--- Client.java
+++ Client.java
@@ -3,6 +3,9 @@ import java.net.*;
 
 // The main class that creates the client socket and communicates with the server
 public class Client {
+    // Declare the running variable as a static volatile field of the Client class
+    private static volatile boolean running = true;
+
     public static void main(String[] args) throws IOException {
         // Create a socket and connect to the server on localhost and port 5000
         Socket socket = new Socket("localhost", 5000);
@@ -10,15 +13,31 @@ public class Client {
         DataInputStream input = new DataInputStream(socket.getInputStream());
         DataOutputStream output = new DataOutputStream(socket.getOutputStream());
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-        // Loop until exit command is sent by the server or typed by the user
-        while (true) {
-            // Read the message from the server and print it
-            String message = input.readUTF();
-            System.out.print(message);
-            // Check if the message is an exit message
-            if (message.equals("Exit command received. Closing the connection.\n")) {
-                break;
+        // Create a new thread for reading and printing the message from the server
+        Thread thread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    // Loop until exit message is received from the server
+                    while (running) {
+                        // Read the message from the server and print it
+                        String message = input.readUTF();
+                        System.out.print(message);
+                        // Check if the message is an exit message
+                        if (message.equals("Exit command received. Closing the connection.\n")) {
+                            // Set the running variable to false to terminate the thread
+                            running = false;
+                        }
+                    }
+                } catch (IOException e) {
+                    System.out.println(e);
+                }
             }
+        });
+        // Start the thread
+        thread.start();
+        // Loop until exit command is typed by the user
+        while (running) {
             // Read the command from the user and send it to the server
             String command = reader.readLine();
             output.writeUTF(command);
